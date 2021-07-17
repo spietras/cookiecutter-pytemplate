9268ef9d5863fdb2170cfbab4b16e25fc17130c0
Sebastian Pietras <seba00767@gmail.com>
2021-07-17 21:08:23 +0200

    Fixed formatting

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,7 +1,6 @@
 import re
 import sys
 
-
 REPO_NAME_EXIT_CODE = 1
 PACKAGE_NAME_EXIT_CODE = 2
 PACKAGE_IMPORT_NAME_EXIT_CODE = 3
@@ -14,30 +13,24 @@ repo_name = "{{ cookiecutter.repo_name }}"
 package_name = "{{ cookiecutter.package_name }}"
 package_import_name = "{{ cookiecutter.package_import_name }}"
 
+
 def regex_error(name, value, regex, exit_code):
     print("ERROR: %s is not a valid %s! REGEX: '%s'" % (value, name, regex))
     sys.exit(exit_code)
 
+
 if not re.match(REPO_NAME_REGEX, repo_name):
-    regex_error(
-        "repository name", 
-        repo_name, 
-        REPO_NAME_REGEX, 
-        REPO_NAME_EXIT_CODE
-    )
+    regex_error("repository name", repo_name, REPO_NAME_REGEX, REPO_NAME_EXIT_CODE)
 
 if not re.match(PACKAGE_NAME_REGEX, package_name):
     regex_error(
-        "package name", 
-        package_name, 
-        PACKAGE_NAME_REGEX, 
-        PACKAGE_NAME_EXIT_CODE
+        "package name", package_name, PACKAGE_NAME_REGEX, PACKAGE_NAME_EXIT_CODE
     )
 
 if not re.match(PACKAGE_IMPORT_NAME_REGEX, package_import_name):
     regex_error(
-        "package import name", 
-        package_import_name, 
-        PACKAGE_IMPORT_NAME_REGEX, 
-        PACKAGE_IMPORT_NAME_EXIT_CODE
+        "package import name",
+        package_import_name,
+        PACKAGE_IMPORT_NAME_REGEX,
+        PACKAGE_IMPORT_NAME_EXIT_CODE,
     )
